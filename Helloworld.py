c22c0d5a8e77ab0246493724848bd8bb9a0dd171
lenatester100 <lena.tester100@gmail.com>
2017-02-07 20:15:23 -0500

    Dance 4 life

---
+++ Helloworld.py
@@ -0,0 +1,46 @@
+print ("Hello_World")
+print ("I am sleepy")
+'''
+Go to sleep
+print ("sleep")
+
+
+
+Test1=float(input("Please Input the score for Test 1..."))
+Test2=float(input("Please Input the score for Test 2..."))
+Test3=float(input("Please Input the score for Test 3..."))
+average=(Test1+Test2+Test3)/3
+print ("The Average of all 3 tests is ", average)
+
+
+print (3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3%2)
+print(3**4)
+print(3//4)
+
+
+print (5.0/9.0)
+print (5.0/9)
+print (5/9.0)
+print (5/9)
+print (9.0/5.0)
+print (9.0/5)
+print (9/5.0)
+print (9/5)
+'''
+
+temp = int (input ("what is the temperature?   "))
+def temperature ():
+
+    if temp  >= 90:
+        print ("hot")
+    elif temp <= 60:
+        print ("chilly")
+    else:
+         print ("Take me to Hawaii")
+
+temperature()
+
