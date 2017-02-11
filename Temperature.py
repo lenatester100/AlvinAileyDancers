cbb9b960d0026860e7def7f2f05e4e283b7cf75f
lenatester100 <lena.tester100@gmail.com>
2017-02-11 15:40:12 -0500

    Dance 4 life

--- Temperature.py
+++ Temperature.py
@@ -1,6 +1,6 @@
 def main ():
     temp = int(input("Enter the temp for fahrenheit"))
-    Celsius = (temp - 32) * 5.0/9.0
+    Celsius = (temp - 32) * 5.0/9.0 #assignment
     print("The temperature in fahrenheit is ")
     print("The  temperature in celsius is ")
     user = input("Would you like to continue?")
